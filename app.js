3ce9b707e57d062c158d2bae0c5959bd65bdc347
VICKYGUPTA02G <vg5709044@gmail.com>
2026-03-28 16:45:23 +0530

    fixed RGB string comparison

--- app.js
+++ app.js
@@ -1,40 +1,38 @@
-let tar=document.querySelector('.item-target');
+let tar = document.querySelector('.item-target');
+let value = 0;
+let score = document.querySelector('.score-btn');
+score.textContent = `${value}`;
 
-tar.style.backgroundColor = `rgb(241, 46, 24)`;
-let value=0;
-let score=document.querySelector('.score-btn');
-score.textContent=`${value}`;
+let color = document.querySelectorAll('.item');
 
-let color=document.querySelectorAll('.item');
+for (let i = 0; i < color.length; i++) {
+    color[i].setAttribute('data-index', `${i + 1}`);
+}
 
-color.forEach(color_generator);
+function startRound() {
+    color.forEach(color_generator);
+
+    let randomIndexofbutton = Math.floor(Math.random() * color.length);
+    let choosebtn = color[randomIndexofbutton];
+    tar.style.backgroundColor = choosebtn.style.backgroundColor;
+}
 
 function color_generator(e) {
- 
-    e.style.backgroundColor = `rgb(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)})`;
+    e.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;
 }
-for(let i=0;i<color.length;i++)
-{
-    color[i].setAttribute('data-index',`${i+1}`);
-};
 
+startRound();
 
-let click_event=document.querySelector('.container');
-click_event.addEventListener('click',count);
+let click_event = document.querySelector('.container');
+click_event.addEventListener('click', count);
 
-function count(e)
-{
-    
-    if(e.target.tagName==='BUTTON')
-    {
-        if(e.target.style.backgroundColor === `${tar.style.backgroundColor}`)
-        {
-            value=value+10;
-            score.textContent=`${value}`;
+function count(e) {
+    if (e.target.tagName === 'BUTTON') {
+        if (e.target.style.backgroundColor === tar.style.backgroundColor) {
+            value = value + 10;
+            score.textContent = `${value}`;
         }
         
-        color.forEach(color_generator);
+        startRound(); 
     }
-
-    
 }
